e9318af46249ab1be39936621b0f4253be78c87d
fhostachy <47024222+fhostachy@users.noreply.github.com>
2019-05-17 08:20:57 +0200

    1.2

--- Assets/Ressources/script/MonsterSystem.cs
+++ Assets/Ressources/script/MonsterSystem.cs
@@ -2,37 +2,46 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MonsterSystem : MonoBehaviour {
+public class MonsterSystem : MonoBehaviour
+{
 
-	public GameObject player;
-	public Animator animator;
+    // Initialisation de tout les object pour l'animation, le canvas GameOver 
+    public GameObject player;
+    public Animator animator;
     public GameObject canvas;
-	public bool isKilling = false;
-	private int state = 0;
+    public bool isKilling = false;
+    private int state = 0;
 
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		transform.position = new Vector3 (player.transform.position.x - 4.5f, -2.6f, -1);	
-		if(isKilling){
-			KillPlayer();
-		}
-	}
+    void Start()
+    {
 
-	public void GoFurther(){
-		state --;
-		animator.SetInteger("State", state);
-	}
+    }
 
-	public void GoCloser(){
-		state ++;
-		animator.SetInteger("State", state);
-	}
-
-	void KillPlayer(){
+    // Update is called once per frame
+    //Déclare la mort du joueur
+    void Update()
+    {
+        transform.position = new Vector3(player.transform.position.x - 4.5f, -2.6f, -1);
+        if (isKilling)
+        {
+            KillPlayer();
+        }
+    }
+    //animation monstre
+    public void GoFurther()
+    {
+        state--;
+        animator.SetInteger("State", state);
+    }
+    // animation monstre
+    public void GoCloser()
+    {
+        state++;
+        animator.SetInteger("State", state);
+    }
+    //Affichage gameover
+    void KillPlayer()
+    {
         canvas.GetComponent<CanvasManager>().GameOver();
     }
 }
